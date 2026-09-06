c283247b9e4af42649ddda26fccd86a11b023bf0
Verity <amaanallen2016@gmail.com>
2026-09-05 21:31:43 -0700

    Add songs data array with multiple entries

---
+++ song.js
@@ -0,0 +1,20 @@
+// songs.js – "EVERY FUCKING SONG IN THE ENTIRE INTERNET" (sample)
+window.songsData = [
+    { title: "Bohemian Rhapsody", artist: "Queen", album: "A Night at the Opera", year: 1975, img: "👑" },
+    { title: "Billie Jean", artist: "Michael Jackson", album: "Thriller", year: 1982, img: "🕺" },
+    { title: "Smells Like Teen Spirit", artist: "Nirvana", album: "Nevermind", year: 1991, img: "🎸" },
+    { title: "Shape of You", artist: "Ed Sheeran", album: "÷", year: 2017, img: "🎤" },
+    { title: "Blinding Lights", artist: "The Weeknd", album: "After Hours", year: 2020, img: "🌃" },
+    { title: "HUMBLE.", artist: "Kendrick Lamar", album: "DAMN.", year: 2017, img: "🎙️" },
+    { title: "Rolling in the Deep", artist: "Adele", album: "21", year: 2011, img: "💔" },
+    { title: "Uptown Funk", artist: "Mark Ronson ft. Bruno Mars", album: "Uptown Special", year: 2014, img: "🕺" },
+    { title: "Old Town Road", artist: "Lil Nas X", album: "7", year: 2019, img: "🐎" },
+    { title: "Shake It Off", artist: "Taylor Swift", album: "1989", year: 2014, img: "💃" },
+    { title: "Lose Yourself", artist: "Eminem", album: "8 Mile", year: 2002, img: "🎤" },
+    { title: "Imagine", artist: "John Lennon", album: "Imagine", year: 1971, img: "🕊️" },
+    { title: "Yesterday", artist: "The Beatles", album: "Help!", year: 1965, img: "🎻" },
+    { title: "Like a Rolling Stone", artist: "Bob Dylan", album: "Highway 61 Revisited", year: 1965, img: "🎸" },
+    { title: "Hotel California", artist: "Eagles", album: "Hotel California", year: 1976, img: "🌴" },
+    { title: "Stairway to Heaven", artist: "Led Zeppelin", album: "Led Zeppelin IV", year: 1971, img: "🎸" },
+    // ... add thousands more
+];
